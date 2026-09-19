29b5c33e40acff522fd201f56def8fc91a2e1d17
Janari Altdorf <jaltdorf@tlu.ee>
2026-09-19 13:18:47 +0300

    feat: add task filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,56 @@
-import { Header } from './components/Header'
-import { TaskCard } from './components/TaskCard'
-import './App.css'
+import { Header } from './components/Header';
+import { TaskCard } from './components/TaskCard';
+import './App.css';
+import { useState } from 'react';
 
-const task1 = {
-  id: 1,
-  title: 'Learn JSX',
-  completed: true,
-}
-
-const task2 = {
-  id: 2,
-  title: 'Practise React state',
-  completed: false,
-}
+const tasks = [
+  {
+    id: 1,
+    title: 'Learn JSX',
+    completed: false,
+  },
+  {
+    id: 2,
+    title: 'Practise React state',
+    completed: false,
+  },
+  {
+    id: 3,
+    title: 'Build a Node.js API',
+    completed: false,
+  },
+];
 
 function App() {
+  const [filter, setFilter] = useState('all');
+
+  let filteredTasks = tasks;
+
+  if (filter === 'completed') {
+    filteredTasks = tasks.filter((task) => task.completed);
+  }
+
+  if (filter === 'not completed') {
+    filteredTasks = tasks.filter((task) => !task.completed);
+  }
+
   return (
     <div>
       <Header />
-      <TaskCard task={task1} />
-      <TaskCard task={task2} />
+      <div>
+        <button onClick={() => setFilter('all')}>All</button>
+        <button onClick={() => setFilter('completed')}>Completed</button>
+        <button onClick={() => setFilter('not completed')}>
+          Not Completed
+        </button>
+      </div>
+      {filteredTasks.length === 0 && <p>No tasks found</p>}
+
+      {filteredTasks.map((task) => (
+        <TaskCard key={task.id} task={task} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
